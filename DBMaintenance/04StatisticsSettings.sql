ba891a5024e35b39c36fa3f4738764192a81117b
Vadivel Mohanakrishnan <vmvadivel@gmail.com>
2018-08-29 19:06:06 +0530

    Auto Create, Update Statistics Settings

--- DBMaintenance/04StatisticsSettings.sql
+++ DBMaintenance/04StatisticsSettings.sql
@@ -7,3 +7,17 @@ WHERE  is_auto_create_stats_on = 0;
 SELECT [name] AS DBName
 FROM   sys.databases
 WHERE  is_auto_update_stats_on = 0;
+
+/*
+-- Generally for better performance I would suggest to keep these 2 features enabled!
+
+USE [master]
+GO
+
+ALTER DATABASE Your_DB_Name SET AUTO_CREATE_STATISTICS ON WITH NO_WAIT;
+GO
+
+ALTER DATABASE Your_DB_Name SET AUTO_UPDATE_STATISTICS ON WITH NO_WAIT;
+GO
+
+*/
