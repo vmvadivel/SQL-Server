07adae02ec30bf1816b4559ea2d2a6c8b441270e
Vadivel Mohanakrishnan <vmvadivel@gmail.com>
2018-08-29 16:52:37 +0530

    Update 03AutoCloseShrinkSetting.sql

--- DBMaintenance/03AutoCloseShrinkSetting.sql
+++ DBMaintenance/03AutoCloseShrinkSetting.sql
@@ -1,3 +1,12 @@
+/*
+
+ If is_auto_close_on = 1 then it means Auto close is enabled in that DB!
+ If is_auto_shrink_on = 1 then it means Auto Shrink is enabled in that DB!
+
+ Both these settings are generally considered to be bad and would decrease the performance of the system!
+
+*/
+
 -- Check if Autoclose is enabled
 SELECT [name] AS DBName
 FROM   sys.databases
@@ -18,3 +27,16 @@ FROM
 WHERE
 	is_auto_close_on = 1 or is_auto_shrink_on = 1;
 
+/*
+-- Disable those features
+
+USE [master]
+GO
+
+ALTER DATABASE Your_DB_Name SET AUTO_CLOSE OFF;
+GO
+
+ALTER DATABASE Your_DB_Name SET AUTO_SHRINK ON WITH NO_WAIT;
+GO
+
+*/		
