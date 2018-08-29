3227c23a98081b2c2c8f1f0be01d924af7c6b1b0
Vadivel Mohanakrishnan <vmvadivel@gmail.com>
2018-08-29 20:17:52 +0530

    Added validation for user databases

--- DBMaintenance/05SystemDB_Drives.sql
+++ DBMaintenance/05SystemDB_Drives.sql
@@ -6,7 +6,7 @@
 */
 
 SELECT 
-	DB_NAME(database_id) AS DBName,
+	DB_NAME(database_id) AS System_DBName,
 --	[name] AS DB_LogicalName,
 	physical_name AS DB_PhysicalName 
 --	,*
@@ -17,3 +17,18 @@ WHERE
 	AND [type] = 0
 	AND DB_NAME(database_id) IN ( 'master', 'model', 'msdb', 'tempdb' );
 	-- AND [name] IN ( 'master', 'model', 'msdb', 'tempdb' );
+
+
+/*
+	Check for User DBs now: User Databases also should not be in C drive
+*/
+
+SELECT 
+	DB_NAME(database_id) AS Users_DBName,
+	physical_name AS DB_PhysicalName 
+FROM    
+	sys.master_files
+WHERE   
+	LOWER(LEFT(physical_name, 1)) = 'c'
+	AND [type] = 0
+	AND DB_NAME(database_id) NOT IN ( 'master', 'model', 'msdb', 'tempdb' );
